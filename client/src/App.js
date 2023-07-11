82d314c583d209d079229010cdc9230b02aaaa75
kpetrosky <kpetrosky36@gmail.com>
2023-07-10 17:39:32 -0700

    trying to get the website to show up

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div>
+      {/* Other content */}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
